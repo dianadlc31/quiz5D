0979598cefafc5d8c14410910e83f2324ac90108
alumno Secundaria <al3201@cedi.local>
2018-11-29 08:33:57 -0600

    Quiz Diana

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,61 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { RegistroPage } from '../registro/registro';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+registro=RegistroPage;
+mail="";
+pass="";
+usuarios=[{mail:"", pass:""}]
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, public storage:Storage) {
+    this.storage.keys()
+    .then (keys =>{
+      if (keys.some(key=> key == 'usuarios')){
+        this.storage.get('usuarios')
+        .then(usuarios => {
+          this.usuarios = JSON.parse(usuarios);
+        })
+      }
+    })
 
   }
 
+
+
+  clickIniciar()
+    {
+      let index = this.usuarios.findIndex (usuarios => usuarios.mail == this.mail );
+      let index2 = this.usuarios.findIndex (usuarios => usuarios.pass ==this.pass);
+      
+      if(index == index2 )
+      {
+        const alert = this.alertCtrl.create({
+          title: 'Su inicio de sesión tuvo exito',
+          buttons:['Ok']
+        });
+        alert.present();
+
+        this.mail="";
+        this.pass="";
+      }
+      else
+      {
+        const alert = this.alertCtrl.create({
+          title: 'Contraseña o email incorrectos',
+          buttons:['Ok']
+        });
+        alert.present();
+      }
+    }
+
+  
+  clickregistro()
+  {
+    this.navCtrl.push(this.registro, {usuarios:this.usuarios})
+  }
 }
---
+++ src/pages/registro/registro.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { HomePage } from '../home/home';
+
+
+/**
+ * Generated class for the RegistroPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-registro',
+  templateUrl: 'registro.html',
+})
+export class RegistroPage {
+
+  mail="";
+  pass="";
+  usuarios=[];
+  home=HomePage;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
+    this.usuarios = this.navParams.get('usuarios');
+
+  }
+
+  
+  clickAgregar()
+  {
+    if(this.mail.length> 0)
+    {
+      
+      this.usuarios.push(
+      {
+        mail: this.mail, 
+        pass: this.pass
+      })
+
+      this.navCtrl.pop();
+      //this.navCtrl.push(this.home, {usuarios:this.usuarios}) 
+     
+    }
+    else if (this.mail.length<=0)
+    {
+      const alert = this.alertCtrl.create({
+        title: 'Oops!',
+        subTitle: 'Tienes que llenar todos los espacios',
+        buttons:['Ok']
+      });
+      alert.present();
+    }
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad RegistroPage');
+  }
+
+  }
+
